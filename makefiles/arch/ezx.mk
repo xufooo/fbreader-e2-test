1b0f8fc8b05b43091a40a411dfbfcb94f7516f2f
ooo <xufooo@gmail.com>
2012-10-23 08:49:01 +0800

    some path fix

--- makefiles/arch/ezx.mk
+++ makefiles/arch/ezx.mk
@@ -1,6 +1,6 @@
-QTDIR = /home/ooo/Compile/blackhawk_e2_sdk_with_gcc-arm-iwmmxt/e2-sdk/qt
-TOOLSDIR = /home/ooo/Compile/blackhawk_e2_sdk_with_gcc-arm-iwmmxt/gcc-arm-iwmmxt/gcc-3.3.6-glibc-2.3.2/arm-linux
-EZXDIR = /home/ooo/Compile/blackhawk_e2_sdk_with_gcc-arm-iwmmxt/e2-sdk/ezx
+QTDIR = /home/ooo/Work/Compile/blackhawk_e2_sdk_with_gcc-arm-iwmmxt/e2-sdk/qt
+TOOLSDIR = /home/ooo/Work/Compile/blackhawk_e2_sdk_with_gcc-arm-iwmmxt/gcc-arm-iwmmxt/gcc-3.3.6-glibc-2.3.2/arm-linux
+EZXDIR = /home/ooo/Work/Compile/blackhawk_e2_sdk_with_gcc-arm-iwmmxt/e2-sdk/ezx
 
 MOC = $(QTDIR)/bin/moc
 RM = rm -rvf
@@ -19,7 +19,7 @@ CFLAGS = -pipe -DQWS -fno-exceptions -fno-rtti -Wall -W -mcpu=iwmmxt -mtune=iwmm
 LDFLAGS = -Wl,-rpath-link,$(QTDIR)/lib:$(EZXDIR)/lib,-rpath,./
 #DEPGEN = $(CC) -MM
 
-EXPATLIBS = -L /home/ooo/Compile/fbreader-0.7.4g/common-lib/lib -lexpat
-ENCALIBS = -L /home/ooo/Compile/fbreader-0.7.4g/common-lib/lib -lenca
-BZIP2LIBS = -L /home/ooo/Compile/fbreader-0.7.4g/common-lib/lib -lbz2
-ZLIBS = -L /home/ooo/Compile/fbreader-0.7.4g/common-lib/lib -lz
+EXPATLIBS = -L /home/ooo/Work/Compile/fbreader-0.7.4g/common-lib/lib -lexpat
+ENCALIBS = -L /home/ooo/Work/Compile/fbreader-0.7.4g/common-lib/lib -lenca
+BZIP2LIBS = -L /home/ooo/Work/Compile/fbreader-0.7.4g/common-lib/lib -lbz2
+ZLIBS = -L /home/ooo/Work/Compile/fbreader-0.7.4g/common-lib/lib -lz
