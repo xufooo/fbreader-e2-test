7b6583458bad5c5a3f397a6e2d08adaa96d42b25
ooo <xufooo@gmail.com>
2012-10-30 19:05:42 +0800

    add -DBETTER_TOOLBAR

--- makefiles/arch/ezx.mk
+++ makefiles/arch/ezx.mk
@@ -15,7 +15,7 @@ CXX	= $(TOOLSDIR)/bin/arm-linux-g++
 # Add -DBETTER_TOOLBAR to CFLAGS to make the toolbar look better (remove bad transparency)
 # You need Motorola's proprietary header called qwallpaper.h
 # You can find it somewhere on the net because I can't ditributed it, copy to zlibrary/ezx/dropin
-CFLAGS = -pipe -DQWS -fno-exceptions -fno-rtti -Wall -W -mcpu=iwmmxt -mtune=iwmmxt -DEZX -DQT_NO_DEBUG -DQT_NO_DRAGANDDROP -DQT_NO_PROPERTIES -DQT_NO_TOOLBUTTON# -Wno-ctor-dtor-privacy -fexpensive-optimizations -fomit-frame-pointer -DINSTALLDIR=\"$(INSTALLDIR)\" -v -Q 
+CFLAGS = -pipe -DQWS -fno-exceptions -fno-rtti -Wall -W -mcpu=iwmmxt -mtune=iwmmxt -DEZX -DQT_NO_DEBUG -DQT_NO_DRAGANDDROP -DQT_NO_PROPERTIES -DQT_NO_TOOLBUTTON -Wno-ctor-dtor-privacy -fexpensive-optimizations -fomit-frame-pointer -DINSTALLDIR=\"$(INSTALLDIR)\" -DBETTER_TOOLBAR #-v -Q 
 LDFLAGS = -Wl,-rpath-link,$(QTDIR)/lib:$(EZXDIR)/lib,-rpath,./
 #DEPGEN = $(CC) -MM
 
